f3caaf8434fe1d904800f30c112af4ee151a9de9
Amisha Singh <amishasingh@example.com>
2025-11-24 02:09:08 +0530

    new

---
+++ src/Pattern/Amisha2.java
@@ -0,0 +1,18 @@
+package Pattern;
+import java.util.*;
+
+
+public class Amisha2 {
+    public static void main(String[] args) {
+        int i , j;
+        int n=4;
+        int m=5;
+           for(i=n; i>=1;i--){
+               for(j=1;j<=i;j++){
+                   System.out.print("*");
+               }
+
+               System.out.println();
+        }
+    }
+}
